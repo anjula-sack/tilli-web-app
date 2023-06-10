76bf9d134db89881bf133a626e3794b4b04a4dae
anjula-sack <anjulashanaka@gmail.com>
2023-06-10 10:05:18 +0530

    Test setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders login screen", () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+
+    const loginElement = screen.getByText(/Login/i);
+    expect(loginElement).toBeInTheDocument();
+  });
+
+  test("renders dashboard screen", () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+
+    // Mock the route change to '/Dashbord'
+    // You can modify the test to match the actual route
+    // or use a different method to simulate navigation
+    window.history.pushState({}, "Test page", "/Dashbord");
+
+    const dashboardElement = screen.getByText(/Dashboard/i);
+    expect(dashboardElement).toBeInTheDocument();
+  });
+
+  // Add more tests for other routes and components...
 });
